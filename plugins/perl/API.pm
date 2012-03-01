0b11f58be9b37669eece42c8638b624415448808
Mitchell Cooper <cooper879@msn.com>
2012-02-29 21:24:56 -0500

    added the beginning of the new Perl API

--- plugins/perl/API.pm
+++ plugins/perl/API.pm
@@ -1,8 +1,43 @@
 # line 1 "API.pm"
-BEGIN {	$INC{'API.pm'} = 1; }
+BEGIN {
+    $INC{"$_.pm"} = 1 foreach qw(
+        API Network Channel User
+        GChat GChat::IRC GChat::IRC::Server
+        GChat::IRC::Channel GChat::IRC::User
+    )
+}
 
 package API;
 
+sub get_module {1
+}
 
+package GChat;
+
+sub EAT_ALL () { Xchat::EAT_ALL }
+
+# GChat->print()
+sub print {
+    my (undef, $text) = (shift, shift);
+    return unless defined $text;
+	Xchat::Internal::print($text);
+}
+
+package Command;
+
+# on Command $cmd => $code, %options
+# this isn't really evented - it just looks like it is.
+sub on {
+    my (undef, $cmd, $code, %options) = @_;
+    my $module = API::get_module(caller) or return;
+    my $hook   = Xchat::Internal::hook_command(
+        $cmd,
+        Xchat::PRI_NORM,
+        $code,
+        $options{help},
+        $options{data}
+    ) or return;
+    GChat->print($hook);
+}
 
 1
